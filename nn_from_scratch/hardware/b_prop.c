a2b9e08fce685eaaa67808c4f9dc4fe7b44cfd14
SimonJ <yconsj@gmail.com>
2024-03-29 15:23:55 +0100

    origin update

---
+++ nn_from_scratch/hardware/b_prop.c
@@ -0,0 +1,45 @@
+/*
+    C file for back propagation
+*/
+
+/* Back propagation function for a single layer*/
+void b_prop()
+{
+    // Compute loss
+    // Compute gradient
+    // use Chain rule
+    // Update parameters
+    return 0;
+}
+
+/* Loss function */
+
+void loss(float predicted, float target)
+{
+    return 0;
+}
+
+/* Compute gradient*/
+
+void gradient(float predicted, float target, float input)
+{
+    float grad = input * (predicted - target); //maybe normalize?
+    
+    return grad;
+
+}
+
+/* Update function to update weights (use gradient decent)*/
+// inputs: weight, gradient, learning
+void update_weights(float *weight, float grad, float rate)
+{
+    *weight -= rate * grad;
+    return 0;
+}
+
+/* Main function for testing*/
+void main()
+{
+
+    return 0;
+}
