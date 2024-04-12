874a92b7acff2c75a8ad3998c6ab14fc0ff60f30
Amin <BlackError76@yahoo.com>
2024-04-12 15:40:43 +0200

    Guarding the header files against duplication
    Adding `#ifdef FILE` and `#def FILE` directives

--- nn_from_scratch/model/convert/c_templates/data.h
+++ nn_from_scratch/model/convert/c_templates/data.h
@@ -1,6 +1,11 @@
+#ifndef DATA_H
+#define DATA_H
+
 #include <stdint.h>
 
 #define N_SAMPLES {n_samples}
 
 extern float {var_name}_x[N_SAMPLES][{input_size}];
 extern float {var_name}_y[N_SAMPLES][{output_size}];
+
+#endif
--- nn_from_scratch/model/convert/c_templates/model.h
+++ nn_from_scratch/model/convert/c_templates/model.h
@@ -1,3 +1,6 @@
+#ifndef MODEL_H
+#define MODEL_H
+
 #include <cstdint>
 
 #define INPUT_SIZE {input_size}
@@ -15,3 +18,5 @@ extern int layers_size[N_LAYERS];
 extern float* layers_weights[N_LAYERS];     // shape: (n_layers)(input_size * output_size)
 extern float* layers_biases[N_LAYERS];      // shape: (n_layers)(output_size)
 extern enum ActivationType layers_activation[N_LAYERS];
+
+#endif
