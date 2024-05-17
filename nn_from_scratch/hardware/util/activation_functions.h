76d515f9bc88dad13ca5eb85f1001b878aad2a18
simon <yconsj@gmail.com>
2024-05-17 22:56:57 +0200

    comment remove

--- nn_from_scratch/hardware/util/activation_functions.h
+++ nn_from_scratch/hardware/util/activation_functions.h
@@ -15,17 +15,3 @@ float linear_deriv(float x);
 ActivationFunc get_activation_func(enum ActivationType activationType);
 ActivationFunc get_activation_func_deriv(enum ActivationType activationType);
 #endif
-/*
-#define linear(x) (x)
-#define relu(x) ((x > 0) ? x : 0)
-#define linear_deriv(x) (1)
-#define relu_deriv(x) ((x > 0) ? 1 : 0)
-// Define macros for each activation function
-#define ACTIVATION_MACRO_LIST \
-    X(LINEAR, linear)         \
-    X(RELU, relu)
-
-#define ACTIVATION_DERIV_MACRO_LIST \
-    X(LINEAR, linear_deriv)         \
-    X(RELU, relu_deriv)
-*/
